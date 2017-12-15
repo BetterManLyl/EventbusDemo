2a1ecd4098bf59d857571b330f3b3e51a1e19946
lyl953147712 <953147712@qq.com>
2017-12-15 14:22:45 +0800

    测试jar依赖

---
+++ mylibrary/src/main/java/runa/mylibrary/MyActivity.java
@@ -0,0 +1,18 @@
+package runa.mylibrary;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author lyl
+ * @date 2017/12/15.
+ */
+
+public class MyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myactivity);
+    }
+}
